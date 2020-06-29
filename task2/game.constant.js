83a38dca5faba85bf01417383aef66dbe502ddb6
Alexandr Trostyanko <atrostyanko@simpligov.com>
2020-06-29 11:12:33 +0300

    add

---
+++ task2/game.constant.js
@@ -0,0 +1,9 @@
+const FIELD_SIZE = 3;
+const ROWS_NUMBER = 200;
+const COLUMNS_NUMBER = 400;
+const BACKGROUND_COLOR = 'rgb(255,228,225)';
+const FIELD_COLOR = 'rgb(199,21,133)';
+const GENERATION_TIME = 0;
+
+const RESSURECTION_FOR_ALIVE = [2, 3];
+const RESSURECTION_FOR_DEAD = [3];
